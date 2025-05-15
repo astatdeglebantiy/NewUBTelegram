ce52ec0ae8443d812458c1913632578986922445
Gleb <glebsh2chko@gmail.com>
2025-05-15 22:21:34 +0300

    update of command syntax (more details in the telegram channel @astatsprojects)

--- command_parser.py
+++ command_parser.py
@@ -31,6 +31,15 @@ class TransformerImpl(lark.Transformer):
     def ESCAPED_STRING(self, token):
         return token[1:-1]
 
+    def TRUE(self, token):
+        return True
+
+    def FALSE(self, token):
+        return False
+
+    def NONE(self, token):
+        return None
+
     def func_call(self, items):
         name = items[1]
         arg_list = items[3] if len(items) > 3 else []
@@ -38,7 +47,7 @@ class TransformerImpl(lark.Transformer):
         return fn(*arg_list)
 
     def arg_list(self, items):
-        return items
+        return [item for item in items if not isinstance(item, lark.Token) or item.type != 'SEP']
 
 
 def parse_command(text: str, _vars=None):
